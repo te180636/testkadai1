28a58f2eb846767b772c9b7d76a23a5b0355c851
te180636 <84059840+te180636@users.noreply.github.com>
2021-09-30 19:26:01 +0900

    Update AppSpringTest.java

--- MyBootApp1/src/test/java/jp/te4a/spring/boot/myapp1/AppSpringTest.java
+++ MyBootApp1/src/test/java/jp/te4a/spring/boot/myapp1/AppSpringTest.java
@@ -20,18 +20,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-
-//SpringBootの起動クラスを指定
 @ContextConfiguration(classes = App.class)
-//クラス内の全メソッドにおいて、実行前にDIコンテナの中身を破棄する設定
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//テストランナー：各テストケース（テストメソッド）を制御する：DIする場合は必須かも
 @ExtendWith(SpringExtension.class)
-//MockおよびWACの自動ロードサーブレット環境を自動作成する
 @AutoConfigureMockMvc
-// テスト時に起動するSpringBootプロジェクトの使用ポート番号を設定する。
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//クラス単位でインスタンスを作成
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppSpringTest {
     @Autowired
@@ -39,7 +32,7 @@ public class AppSpringTest {
     @Autowired
     WebApplicationContext wac;
     @BeforeAll
-    public void テスト前処理(){
+    public void テスト前(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/templates");
         viewResolver.setSuffix(".html");
@@ -48,7 +41,7 @@ public class AppSpringTest {
     }
 
     @Test
-    public void トップページ表示() throws Exception{
+    public void 表示() throws Exception{
         MvcResult result = mockMvc.perform(get("/"))
         .andExpect(status().is2xxSuccessful())
         .andExpect(content().string("Hello Spring World"))
