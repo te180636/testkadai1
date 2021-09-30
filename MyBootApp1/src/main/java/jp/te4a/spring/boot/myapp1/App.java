ef6fb9c1009829bca8e1647729314c4492a0d51c
te180636 <84059840+te180636@users.noreply.github.com>
2021-09-30 19:25:17 +0900

    Update App.java

--- MyBootApp1/src/main/java/jp/te4a/spring/boot/myapp1/App.java
+++ MyBootApp1/src/main/java/jp/te4a/spring/boot/myapp1/App.java
@@ -6,10 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Hello world!sprint-boot
- *
- */
  @Configuration
  @EnableAutoConfiguration
  @ComponentScan
@@ -17,7 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext ctx = 
-            SpringApplication.run(App.class,args);
+        ApplicationContext ctx = SpringApplication.run(App.class,args);
     }
 }
